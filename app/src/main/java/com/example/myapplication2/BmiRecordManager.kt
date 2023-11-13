33f59b499c147921bc58892481de701e1f29e22d
Hania111 <chococookimonster@gmail.com>
2023-11-13 12:24:48 +0100

    added current data to bmi record

--- app/src/main/java/com/example/myapplication2/BmiRecordManager.kt
+++ app/src/main/java/com/example/myapplication2/BmiRecordManager.kt
@@ -6,6 +6,9 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.preferencesKey
 import com.example.myapplication2.unit.system.UnitSystem
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class BmiRecordManager(private val context: Context,
                        private val dataStore: DataStore<Preferences>) {
@@ -20,7 +23,13 @@ class BmiRecordManager(private val context: Context,
 
     fun createBmiRecord(height: Double, weight: Double, unitSystem: UnitSystem): BmiRecord {
         val bmi = bmiCalculator.calculateBMI(height, weight, unitSystem)
-        return BmiRecord(bmi, weight, height, unitSystem.unitWeight(), unitSystem.unitHeight())
+        val currentDate = getCurrentDate()
+        return BmiRecord(bmi, weight, height, unitSystem.unitWeight(), unitSystem.unitHeight(), currentDate)
+    }
+
+    private fun getCurrentDate(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        return dateFormat.format(Date())
     }
 
     fun interpretBMI(bmi: Double): Pair<String, Int> {
